da40b05fcf7b1619b8dc0f8781271191f316979a
aftabuddin3479gmail <75784579+aftabuddin3479gmail@users.noreply.github.com>
2020-12-10 16:43:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,12 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "AAHTvjdfKyXRPh9QqoM6hBqxUDqw34f572k"
+    APP_ID = "2206386"
+    API_HASH = "cbb279159c065dbb3ab1e3160b004518"
+    DATABASE_URL = "postgres://dvchwqfkuuenpa:a892026422fbb220e3e64d31ab0b9a1807c483a00826d5d692b10e52b9b55c7f@ec2-18-233-207-22.compute-1.amazonaws.com:5432/da6dt8b7ukqei5"
+    SUDO_USERS = "1366454553" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
+
 
 
 class BotCommands:
