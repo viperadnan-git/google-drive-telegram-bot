e56bd6b32cf86cf90687993f1a42305b1c4ab3d3
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-11 13:40:02 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "6088041437:AAF_MqVbJYDbKbWM3qY-BnQxgY1ydDT9KRI"
+    APP_ID = "10112326"
+    API_HASH = "76b4e277c75aaf991589d78eca42946a"
     DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    SUDO_USERS = "5227230295" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "564756005768-dn95foti9c9kvglan276ckvunn1gebc3.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "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"
 
 
 class BotCommands:
