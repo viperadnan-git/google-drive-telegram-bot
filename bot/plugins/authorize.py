5f8a3c15e8fdcccc926e76e9992478b892e6bf67
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-11 13:40:56 +0630

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+REDIRECT_URI = "https://khaine-g-drive.onrender.com"
 
 flow = None
 
