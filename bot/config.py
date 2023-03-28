f36a0fb35453c192bd8658e32733dd77935794b7
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-03-28 16:27:06 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -67,7 +67,7 @@ class Messages:
     
     INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
     
-    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
+    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and copy final website same like http://localhost/?code= .**\n__Visit the URL > Allow permissions >  copy full website  > Send it here__"
     
     DOWNLOAD_TG_FILE = "📥 **Downloading File...**\n**Filename:** ```{}```\n**Size:** ```{}```\n**MimeType:** ```{}```"
     
